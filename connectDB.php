afeffa57ab49563b3f0f1e5a7a10153f53c48fc6
ndev0008 <ndev0008@student.monash.edu>
2023-09-01 20:49:29 +1000

    initial commit

---
+++ connectDB.php
@@ -0,0 +1,31 @@
+<?php
+  $con = mysqli_connect('localhost', 'root', 'root', 'unityaccess');
+  if (mysqli_connect_errno()) {
+    echo "1: Connection failed";
+    exit();
+  }
+
+  $energyconsumption = "SELECT * FROM Suburb";
+  $check = mysqli_query($con, $energyconsumption) or die("2: query failed");
+
+  // Create an array to store all the rows
+  $rows = array();
+
+  // Loop through each row and fetch the data
+  while ($row = mysqli_fetch_assoc($check)) {
+    // Add each row to the array
+    $rows[] = $row;
+  }
+
+  // Check if any rows were found
+  if (count($rows) > 0) {
+    // Create an associative array to store the result
+    $result = array("status" => "success", "data" => $rows);
+  } else {
+    // No rows found, still return an empty data array
+    $result = array("status" => "error", "message" => "No data found in the 'Suburb' table.", "data" => $rows);
+  }
+
+  // Convert the result array to JSON and echo it
+  echo json_encode($result);
+?>
